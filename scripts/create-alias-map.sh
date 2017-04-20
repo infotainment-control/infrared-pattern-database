b5d7b603492114f350161d2069f9d1902375713c
mark.bank <mark.bank@ibm.com>
2017-04-19 20:35:45 -0400

    standardization script works well enough for v0.1

--- scripts/create-alias-map.sh
+++ scripts/create-alias-map.sh
@@ -1,43 +1,30 @@
 #!/usr/local/bin/bash
 
 alias_file="../commands/commands-and-aliases.csv"
+map_file="../commands/alias-map.csv"
 
 IFS=','
+OFS=','
 
-
-#declare -a map
-#k="greeting+"
-#sed 's:+:my_plus:g' <<<${k}
-#map["${k}"]="hello"
-#printf '%s\n' "${map[@]}"
+rm -rf ${map_file}
 
 declare -a alias_to_standard_map
 
 while read key aliases; do
 
-  # identity entry
-  alias_to_standard_map["${key}"]="${key}"
-
-  #echo "key=${key}"
-  #echo "aliases=${aliases}"
+  echo "${key},${key}" >> ${map_file}
 
   for alias in $(echo "${aliases}"); do
+    echo "${alias},${key}" >> ${map_file}
     #echo "  alias=${alias}"
-    cleaned=$(sed 's:+:my_plus:g; s:-:my_minus:g; s:/:my_forward_slash:g; s:\\:my_back_slash:g' <<<${alias})
+    #cleaned=$(sed 's:+:my_plus:g; s:-:my_minus:g; s:/:my_forward_slash:g; s:\\:my_back_slash:g' <<<${alias})
     #echo "  clean=${cleaned}"
     #alias_to_standard_map["${alias}"]="${key}"
-    alias_to_standard_map["${cleaned}"]="${key}"
+    #alias_to_standard_map["${cleaned}"]="${key}"
     #echo "assigned ${alias} the standard ${key}"
     #echo "assigned ${cleaned} the standard ${key}"
   done
 
 done < ${alias_file}
 
-printf '%s\n' "${alias_to_standard_map[@]}"
-
-#$ ./create-alias-map.sh 
-#  alias=PWR
-#assigned PWR to POWER
-#  alias=VOL+
-#./create-alias-map.sh: line 19: VOL+: syntax error: operand expected (error token is "+")
-#VOLUME_UP
+#printf '%s\n' "${alias_to_standard_map[@]}"
--- scripts/standardize.sh
+++ scripts/standardize.sh
@@ -1,17 +1,37 @@
 #!/usr/local/bin/bash
+version=0.1
+version_tag="automated_alias_${version}"
+
+map_file="../../../../commands/alias-map.csv"
+
+standardize () {
+  to_standardize=$1
+  standard_file=$2
+
+  pwd -P
+
+  while read device_id command_term; do
+    entry=$(grep -i -m 1 "^${command_term}," ${map_file})
+    if [ ${entry} ]; then
+      IFS=',' read raw_command standard <<< ${entry}
+      #echo "found match: ${command_term} -> ${standard}"
+
+      printf "%s\t%s\t%s\t%s\n" "${device_id}" "${standard}" "${command_term}" "${version_tag}" >> ${standard_file}
+
+    fi
+  done < ${to_standardize}
+}
 
 
-#to_standardize="standardization/bucket_1_to_200/all/1"
-to_standardize="standardization/bucket_1_to_200/all/1"
-map_file="../commands/alias-map.csv"
+for bucket in $(stat -f %N standardization/*); do
+  name=$(basename ${bucket})
+  echo "processing bucket ${name}"
+  
+  cd ${bucket}/all
+  
+  for device_id in $(stat -f %N *); do
+    standardize ${device_id} ../standardized/${device_id}
+  done
 
-while read device_id command_term; do
-  entry=$(grep -i -m 1 "^${command_term}," ${map_file})
-  if [ ${entry} ]; then
-    IFS=',' read raw_command standard <<< ${entry}
-    #standard=$(awk -F',' '{print 
-    #echo "found match: ${command_term} -> ${entry}"
-    echo "found match: ${command_term} -> ${standard}"
-  fi
-done < ${to_standardize}
-
+  cd ../../..
+done
